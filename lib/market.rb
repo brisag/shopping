938fb92852e2bbd15ae8b0d223987b0550af6ddf
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-20 10:38:31 -0700

    Refactor inventory

--- lib/market.rb
+++ lib/market.rb
@@ -1,8 +1,9 @@
 class Market
-  attr_reader :name, :vendors
+  attr_reader :name, :vendors, :date
   def initialize(name)
     @name = name
     @vendors = []
+    @date = Time.new.strftime("%m-%d-%Y").freeze
   end
 
   def add_vendor(vendor)
@@ -21,15 +22,18 @@ class Market
     end
   end
 
+  def quantity(item)
+  @vendors.sum do |vendor|
+    vendor.inventory[item]
+  end
+end
+
   def total_inventory
-    total = {}
-    items = @vendors.flat_map {|vendor| vendor.inventory.keys}.uniq
-    items.each do |item|
-      vendors_for_item = vendors_that_sell(item)
-      total_amount = vendors_for_item.sum {|vendor| vendor.inventory[item]}
-      total[item] = {quantity: total_amount, vendors: vendors_for_item}
+    hash = {}
+    list_of_names.each do |item|
+      hash[item] = {quantity:quantity(item), vendors:vendors_that_sell(item)}
     end
-    total
+    hash
   end
 
   def list_of_names
@@ -48,8 +52,6 @@ class Market
     end.sort
   end
 
-
-#overstocked if it is sold by more than 1 vendor AND the total quantity is greater than 50.
   def overstocked_items
     total_inventory.map do |item, data|
       item if data[:vendors].count > 1 && data[:quantity] > 50
